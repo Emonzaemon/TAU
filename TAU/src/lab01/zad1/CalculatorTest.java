a62942460160c8388cd17e86dcdba6b6ebd5f719
s11718 <s11718@lab3l-2.pjwstk.edu.pl>
2016-10-09 11:40:53 +0200

    first commit

---
+++ TAU/src/lab01/zad1/CalculatorTest.java
@@ -0,0 +1,33 @@
+package lab01.zad1;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+	Calculator calculator = new Calculator();
+	
+	@Test
+	public void calculatorTestAdd(){
+		assertEquals(4,calculator.add(1, 3));
+	}
+	@Test
+	public void calculatorTestSub(){
+		assertEquals(3,calculator.sub(6, 3));
+	}
+	@Test
+	public void calculatorTestMulti(){
+		assertEquals(10,calculator.multi(2, 5));
+	}
+	@Test
+	public void calculatorTestDiv(){
+		assertEquals(5,calculator.div(10, 2));
+	}
+	@Test
+	public void calculatorTestGreater(){
+		assertTrue(calculator.greater(3, 2));
+		
+	}
+	
+	
+}
